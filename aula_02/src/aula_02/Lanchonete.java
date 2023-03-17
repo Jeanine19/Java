30e1747e92f3f5700f3843dd8ea2a5808258b81d
Jeanine <jeanine.a.santiago@gmail.com>
2023-03-17 17:11:45 -0300

    aula03

--- aula_02/src/aula_02/Lanchonete.java
+++ aula_02/src/aula_02/Lanchonete.java
@@ -6,21 +6,53 @@ public class Lanchonete {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner leia = new Scanner (System.in);
+		Scanner leia = new Scanner(System.in);
+		float preco;
+		int produto;
+		int quantidade;
 		
-		int cod;
-		System.out.println("Código do produto");
-        System.out.println("1\tCachorro quente");
-        System.out.println("2\tX-Salada");
-        System.out.println("3\tX-Bacon");
-        System.out.println("4\tBauru");
-        System.out.println("5\tRefrigerante");
-        System.out.println("6\tSuco de laranja\n\n");
-        System.out.println("Digite a faixa etária: ");
-      
-        cod = leia.nextInt("digite o codigo do produto: ");
-        
-		
+
+		System.out.println("produto");
+		System.out.println("1\tCachorro Quente: R$ 10.00");
+		System.out.println("2\tX-Salada: R$ 15.00");
+		System.out.println("3\tX-Bacon: R$ 18.00");
+		System.out.println("4\tBauru: R$ 12.00");
+		System.out.println("5\tRefrigerante: R$ 8.00");
+		System.out.println("6\tSuco de laranja: R$ 13.00\n\n");
+
+		System.out.println("Digite o codigo do produto: ");
+		produto = leia.nextInt();
+
+		System.out.println("Digite a quantidade: ");
+		quantidade = leia.nextInt();
+
+		switch (produto) {
+		case 1:
+			System.out.println("O valor do produto é: " + (preco = 10 * quantidade));
+			break;
+		case 2:
+			System.out.println("O valor do produto é: " + (preco = 15 * quantidade));
+			break;
+		case 3:
+			System.out.println("O valor do produto é: " + (preco = 18 * quantidade));
+			break;
+		case 4:
+			System.out.println("O valor do produto é: " + (preco = 19 * quantidade));
+			break;
+		case 5:
+			System.out.println("O valor do produto é: " + (preco = 8 * quantidade));
+			break;
+		case 6:
+			System.out.println("O valor do produto é: " + (preco = 13 * quantidade));
+			break;
+		default:
+			if (produto >= 7)
+				System.out.println("Produto Invalido");
+
+			System.out.println("Valor Total: ");
+     
+		}
+
 	}
 
 }
