94135fa28c73bbb65c09f961e9662f33fe0b720d
Jeanine <jeanine.a.santiago@gmail.com>
2023-03-22 20:02:12 -0300

    aula05

--- aula5/src/aula5/Exercicio3.java
+++ aula5/src/aula5/Exercicio3.java
@@ -1,9 +1,31 @@
 package aula5;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Exercicio3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		Scanner leia = new Scanner(System.in);
+		
+		Set<Integer> numeros = new HashSet<Integer>();
+		
+		for(int contador = 0; contador < 10; contador ++) {
+			System.out.println("Digite um número: ");
+			numeros.add(leia.nextInt());
+		}
+		
+		numeros.add(null);
+		
+		System.out.println("Listar os Elementos do Conjunto");
+		
+		for(var numero : numeros)
+			System.out.println(numero);
+		
+		leia.close();
 
 	}
 
