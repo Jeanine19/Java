546157e81124f5069a50ded752f3f9a8641347b0
Jeanine <jeanine.a.santiago@gmail.com>
2023-03-23 17:17:46 -0300

    banco

---
+++ aula6/src/aula06/calculos/Calculadora.java
@@ -0,0 +1,21 @@
+package aula06.calculos;
+
+public class Calculadora {
+
+	public double soma(double numero1, double numero2) {
+		return numero1 + numero2;
+	}
+	
+	public double subtracao(double numero1, double numero2) {
+		return numero1 - numero2;
+	}
+	
+	public double multiplicacao(double numero1, double numero2) {
+		return numero1 * numero2;
+	}
+	
+	public double divisao(double numero1, double numero2) {
+		return numero1 / numero2;
+
+}
+}
